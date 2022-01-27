66465d9e87ff0d115b8a52fa4a4bebad704445c6
artmakim <artmakim@gmailcom>
2022-01-27 16:35:52 +0300

    Урок с открытием фото и видео

--- read.py
+++ read.py
@@ -1,6 +1,16 @@
 import cv2 as cv
+from numpy import true_divide
 
-img = cv.imread('Photos/cat.jpg')
+# img = cv.imread('Photos/cat_large.jpg')
+# cv.imshow('cat',   img)
+#cv.waitKey(2000)
 
-cv.imshow('cat',   img)
-cv.waitKey(0)
+capture = cv.VideoCapture('Videos/dog.mp4') #(0) - веб камера
+while True:
+    isTrue, frame = capture.read()
+    cv.imshow('Vid',frame)
+
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+capture.release()
+cv.destroyAllWindows()
