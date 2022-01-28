2e19334d550b2d34d3dad262ad3b66f01dbcb3e4
artmakim <artmakim@gmailcom>
2022-01-28 19:34:49 +0300

    draw

--- draw.py
+++ draw.py
@@ -1,2 +1,17 @@
 import cv2 as cv
 import numpy as np
+
+blank=np.zeros((500,500,3),dtype='uint8')
+# 1. Paint certain colour
+
+#blank[200:300,300:400]=0,0,255
+#cv.imshow('blank',blank)
+
+
+# cv.rectangle(blank, (0,0), (blank.shape[1]//2,blank.shape[1]//2), (0,255,0), thickness=-1)
+# cv.imshow('blank',blank)
+
+#cv.circle(blank, (blank.shape[1]//2,blank.shape[1]//2), 50, (0,255,0), thickness=1)
+cv.putText(blank,'Hello',(255,255),cv.FONT_HERSHEY_TRIPLEX,1.0,(0,255,0),2)
+cv.imshow('blank',blank)
+cv.waitKey(0)
--- read.py
+++ read.py
@@ -1,16 +1,25 @@
+from turtle import width
 import cv2 as cv
 from numpy import true_divide
 
-# img = cv.imread('Photos/cat_large.jpg')
-# cv.imshow('cat',   img)
-#cv.waitKey(2000)
+img = cv.imread('Photos/cat_large.jpg')
+cv.imshow('cat',   img)
 
-capture = cv.VideoCapture('Videos/dog.mp4') #(0) - веб камера
-while True:
-    isTrue, frame = capture.read()
-    cv.imshow('Vid',frame)
+def rescaleFrame(frame,scale=0.75):
+    width=frame.shape[1]*scale
+    height=frame.shape[0]*scale
+    dimensions=(width,height)
+    
+    return cv.resize(frame,dimensions,interpolation=cv.INTER_AREA)
 
-    if cv.waitKey(20) & 0xFF==ord('d'):
-        break
-capture.release()
-cv.destroyAllWindows()
+cv.waitKey(4000)
+
+# capture = cv.VideoCapture('Videos/dog.mp4') #(0) - веб камера
+# while True:
+#     isTrue, frame = capture.read()
+#     cv.imshow('Vid',frame)
+
+#     if cv.waitKey(20) & 0xFF==ord('d'):
+#         break
+# capture.release()
+# cv.destroyAllWindows()
